e03fd472757c96b09b7bf761484243159cd31e7a
davidsiers <davidsiers@gmail.com>
2024-12-30 16:42:29 -0700

    update to tab bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,30 @@
 import { Stack, Tabs } from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
 
 export default function RootLayout() {
-  return <Tabs>
+  return <Tabs screenOptions={{
+    headerShadowVisible: true
+    }}>
     <Tabs.Screen name="index" options={{href: null}} />
-    <Tabs.Screen name="discover" />
-    <Tabs.Screen name="events" />
-    <Tabs.Screen name="sermons" />
-    <Tabs.Screen name="account" />
+    <Tabs.Screen name="discover" options={{
+      title: 'Discover',
+      tabBarIcon: ({color, size}) => 
+        <Ionicons name="compass" size={size} color={color} />
+      }} />
+    <Tabs.Screen name="events" options={{
+      title: 'Events',
+      tabBarIcon: ({color, size}) => 
+        <Ionicons name="albums" size={size} color={color} />
+      }} />
+    <Tabs.Screen name="sermons" options={{
+      title: 'Sermons',
+      tabBarIcon: ({color, size}) => 
+        <Ionicons name="videocam" size={size} color={color} />
+      }} />
+    <Tabs.Screen name="account" options={{
+      title: 'Account',
+      tabBarIcon: ({color, size}) => 
+        <Ionicons name="person-circle" size={size} color={color} />
+      }} />
   </Tabs>;
 }
